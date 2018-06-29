b16a932ea7a1065601b5e7c3f9e56a8668a4a726
chenjunlin <chenjunlin@zljr100.com>
2018-06-29 17:54:18 +0800

    flutter-login

---
+++ lib/main.dart
@@ -0,0 +1,126 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:http/http.dart' as http;
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      theme: new ThemeData(primarySwatch: Colors.blue),
+      home: new LoginPage(),
+    );
+  }
+}
+
+class LoginPage extends StatefulWidget {
+  @override
+  State createState() => new LoginPageState();
+}
+
+class LoginPageState extends State<LoginPage>
+    with SingleTickerProviderStateMixin {
+  Animation<double> _iconAnimation;
+  AnimationController _iconAnimationController;
+
+  @override
+  void initState() {
+    super.initState();
+    _iconAnimationController = new AnimationController(
+        vsync: this, duration: new Duration(milliseconds: 500));
+    _iconAnimation = new CurvedAnimation(
+      parent: _iconAnimationController,
+      curve: Curves.bounceOut,
+    );
+    _iconAnimation.addListener(() => this.setState(() {}));
+    _iconAnimationController.forward();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var url = "http://192.168.40.8:8080/ajaxValidation.do";
+    http.post(url, body: {"username": "admin", "password":"1","company":"1","checkCode":"1233"})
+        .then((response) {
+      print("Response status: ${response.statusCode}");
+      print("Response body: ${response.body}");
+    });
+
+    return new Scaffold(
+      backgroundColor: Colors.blue,
+      body: new Stack(fit: StackFit.expand, children: <Widget>[
+        new Image(
+          image: new AssetImage("assets/txting.jpeg"),
+          fit: BoxFit.cover,
+          colorBlendMode: BlendMode.darken,
+          color: Colors.black87,
+        ),
+        new Theme(
+          data: new ThemeData(
+              brightness: Brightness.dark,
+              inputDecorationTheme: new InputDecorationTheme(
+                // hintStyle: new TextStyle(color: Colors.blue, fontSize: 20.0),
+                labelStyle:
+                new TextStyle(color: Colors.tealAccent, fontSize: 18.0),
+              )),
+          isMaterialAppTheme: true,
+          child: new ListView(
+            children: <Widget>[
+              new Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  new FlutterLogo(
+                    size: _iconAnimation.value * 140.0,
+                  ),
+
+                  new Container(
+                    padding: const EdgeInsets.all(40.0),
+                    child: new Form(
+                      autovalidate: true,
+                      child: new Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: <Widget>[
+                          new TextFormField(
+                            decoration: new InputDecoration(
+                              icon: new Icon(
+                                Icons.phone,
+                              ),
+                                labelText: "输入用户名", fillColor: Colors.white),
+                            keyboardType: TextInputType.text,
+                          ),
+                          new TextFormField(
+                            decoration: new InputDecoration(
+                              icon: new Icon(
+                                Icons.contact_phone,
+                              ),
+                              labelText: "输入密码",
+                            ),
+                            obscureText: true,
+                            keyboardType: TextInputType.text,
+                          ),
+                          new Padding(
+                            padding: const EdgeInsets.only(top: 60.0),
+                          ),
+                          new MaterialButton(
+                            height: 50.0,
+                            minWidth: 150.0,
+                            color: Colors.green,
+                            splashColor: Colors.teal,
+                            textColor: Colors.white,
+                            child: new Icon(FontAwesomeIcons.signInAlt),
+                            onPressed: () {},
+                          )
+                        ],
+
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ],
+          ),
+        ),
+      ]),
+    );
+  }
+}
